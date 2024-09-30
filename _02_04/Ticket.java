19793c0c662d6fa653068eec33bd5ebd60e54ff6
Vishnu <vishnu.v2704@gmail.com>
2024-09-30 16:21:39 +0000

    modified:   _02_04/Ticket.java

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -12,8 +12,31 @@ public class Ticket {
 
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public void setDestination(String destination){
+    this.destination = destination;
+  }
+
+  public void setPrice(double price){
+    this.price = price;
+  }
+
+  public void setIsReturn(boolean isReturn){
+    this.isReturn = isReturn;
+  }
 
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+
+  public String getDestination(){
+    return destination;
+  }
+
+  public double getPrice(){
+    return price;
+  }
+
+  public boolean getIsReturn(){
+    return isReturn;
+  }
 
 }
