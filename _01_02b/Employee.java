1598bdea921490845e2d6aa1e84ad16687b5fd0d
Vishnu <vishnu.v2704@gmail.com>
2024-09-30 15:27:32 +0000

    modified:   _01_02b/Employee.java

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -5,29 +5,33 @@ public class Employee {
   public static void main(String[] args) {
 
     // Create a variable called age of type int and assign it the value 29.
+    int age = 29;
 
     // Print the age variable to the console.
-
+    System.out.println(age);
     // Create a variable called isAManager of type boolean and assign it the value
+
+    boolean isAmanager = true;
     // true.
-
+    
     // Print the isAManager variable to the console.
-
+    System.out.println(isAmanager);
     // Create a variable called yearsOfService of type double and assign it the
     // value 2.5.
-
+    double yearsOfService = 2.5;
+  
     // Print the yearsOfService variable to the console.
-
+    System.out.println(yearsOfService);
     // Create a variable called baseSalary of type int and assign it the value 3000.
-
+    int baseSalary = 3000;
     // Create a variable called overtimePayment of type int and assign it the value
     // 40.
-
+    int overtimePayment = 40;
     // Create a variable called totalPayment of type int and assign it to the value
     // of baseSalary added to overtimePayment.
-
+    int totalPayment = baseSalary + overtimePayment;
     // Print the totalPayment variable to the console.
-
+    System.out.println(totalPayment);
     // Create three variables all of type double on a single line.
     // They should be called firstBonus, secondBonus and thirdBonus and they should
     // be assigned the values 10.00, 22.00 and 35.00.
