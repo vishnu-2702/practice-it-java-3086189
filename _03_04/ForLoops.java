a9935a731e2c6a55fa73d504b4a2225e794f8981
Vishnu <vishnu.v2704@gmail.com>
2024-10-02 12:26:50 +0000

    modified:   _03_04/ForLoops.java

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,11 +4,19 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
+    for (int i= 0; i<5; i++){
+      System.out.println("I Love for Loops");
+    }
 
     // Write a for loop that prints out the numbers 1 to 10
+    for (int i = 1 ; i<=10; i++){
+      System.out.println(i);
+    }
 
     // Write a for loop that prints out the numbers 10 to 1
-    
+    for(int i=10; i>0; i--){
+      System.out.println(i);
+    }
 
   }
 
